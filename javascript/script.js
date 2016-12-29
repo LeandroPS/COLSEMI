e62f9cc4f851095fe8925ea1fe7ba18ef3822c28
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2016-12-29 14:48:03 -0200

    all I have so far

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+$(function(){
+	var elementPosition = $('div.menu').offset();
+
+	$(window).scroll(function(){
+		if($(window).scrollTop() > elementPosition.top){
+				$('div.menu, div.header').addClass("fixed-menu");
+		} else {
+				$('div.menu, div.header').removeClass("fixed-menu");
+		}    
+	});
+	
+	$("div.menu").click(function(){
+		if($(window).width() < 800 && $(window).scrollTop() < 530){
+			/*$('html, body').animate({
+					scrollTop: 530
+			 }, 2000, function(){*/
+				$("div.menu > ul").toggleClass("show");
+			//});
+			
+		}else if($(window).width() < 800){
+			$("div.menu > ul").toggleClass("show");
+		}
+		
+	});
+});
